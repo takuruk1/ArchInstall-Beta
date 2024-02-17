fcb4db7a4a9c50586b6d2baf8216ec58c6c6ad06
takuruk1 <takuru1ki@gmail.com>
2024-02-17 18:24:09 +0000

    Partitioning Step Added

---
+++ Step1.sh
@@ -0,0 +1,11 @@
+echo -e "\nFormatting drive, please wait.\n"
+wipefs --all /dev/
+echo -e "\nCreating BOOT partition.\n"
+sgdisk -n 0:0:+512M -t 0:ef00 -c 0:BOOT /dev/nvme0n1
+echo -e "\nCreating ROOT partition\n"
+sgdisk -n 0:0:0 -t 0:8304 -c 0:ROOT /dev/nvme0n1
+echo -e "\nCreating ESP partition\n"
+mkfs.fat -F 32 -n "ESP" /dev/nvme0n1p1
+echo -e "\nCreating System partition\n"
+mkfs.ext4 -L "System" -F /dev/nvme0n1p2
+echo -e "\nFinished, please run the next script.\n"
